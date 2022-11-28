9cc69997a75e6a97f76a8539260765c28960dca9
payne <82342866+sonerpayne@users.noreply.github.com>
2022-11-28 04:15:13 +0300

    Added summary to IParticleTrigger

--- ParticleTriggerEvents/IParticleTrigger.cs
+++ ParticleTriggerEvents/IParticleTrigger.cs
@@ -3,6 +3,9 @@ namespace ParticleTriggerEvents
     // Implement this for collision detection.
     public interface IParticleTrigger
     {
+        /// <summary>
+        /// Particle collided with object.
+        /// </summary>
         UnityEngine.ParticleSystem.Particle CollidedParticle { get; set; }
         void OnParticleTriggerEnter2D();
     }
